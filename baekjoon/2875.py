487bd20d3abfabaea1a3397386ce0da9e2b01fda
kweek0pen <158253840+kweek0pen@users.noreply.github.com>
2025-01-21 23:49:49 +0900

    Update 2875.py

--- baekjoon/2875.py
+++ baekjoon/2875.py
@@ -1,30 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-
-def Teams(N, M, K):
-    fTeams = N // 2 #fTeams는 여자를 둘씩 나눴을 때 몫
-    if (M > fTeams): #남자가 남을 때
-        mTeams = fTeams
-        notTeams = M - mTeams
-        if(K > notTeams):
-            while(K > notTeams):
-                notTeams += 3
-                fTeams -= 1
-            print(fTeams)
-        else :
-            print(fTeams)
-        
-    else : #여자가 남을 때
-        mTeams = M
-        notTeams = N - 2 *fTeams
-        if (K > notTeams):
-            while(K > notTeams):
-                notTeams += 3
-                mTeams -= 1
-            print(mTeams)
-        else:
-            print(mTeams)
-
 N, M, K = map(int, input().split())
-Teams(N, M, K)
+teams = 0
+while True:
+    N -= 2
+    M -= 1
+    if N<0 or M<0 or (N+M)<K:
+        break
+    teams += 1
+print(teams)
